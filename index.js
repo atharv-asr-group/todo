435b08f552fdd4f3a1c62cf5bd403b513e4589da
unknown <atharv21240@iiitd.ac.in>
2024-01-02 11:41:25 +0530

    basic frontend, fetch and console log todo array

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
+const cors=require('cors');
+app.use(cors());
 app.use(bodyParser.json());
 
 var todos=[];
